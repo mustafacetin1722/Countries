5100114dffedb23b739397e570bd93bee5a693ab
xnzar <mustafacetin2043@gmail.com>
2024-03-09 13:16:45 +0300

    proje-yukleme

---
+++ src/main/java/com/mustafa/countriesbe/CountriesbeApplication.java
@@ -0,0 +1,13 @@
+package com.mustafa.countriesbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountriesbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountriesbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mustafa/countriesbe/CountriesbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafa.countriesbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
